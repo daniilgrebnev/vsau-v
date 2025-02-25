fceda1db31e9a8b2e834b6f5dc2b2fac951cbff4
daniilgrebnev <danilgrebneff@yandex.ru>
2025-02-25 09:56:26 +0300

    go ahead

--- app/app/src/layout/Layout.tsx
+++ app/app/src/layout/Layout.tsx
@@ -1,26 +1,22 @@
-import { useLocation } from 'react-router-dom'
-import { Widget } from '../components/Widget'
 import { NavBar } from './NavBar'
 import { Sidebar } from './sidebar/Sidebar'
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
-	const { pathname } = useLocation()
-	const imgIsVisible = () => {
-		switch (pathname) {
-			case '/':
-				return true
-			case '/contacts':
-				return true
-			default:
-				return false
-		}
-	}
+	// const { pathname } = useLocation()
+	// const imgIsVisible = () => {
+	// 	switch (pathname) {
+	// 		case '/':
+	// 			return true
+	// 		case '/contacts':
+	// 			return true
+	// 		default:
+	// 			return false
+	// 	}
+	// }
 	return (
 		<div className='flex h-[100vh] w-[100vw] '>
 			<Sidebar />
 			<section className='min-h-screen flex w-full flex-col h-[100vh] max-h-[100vh] overflow-y-auto'>
-				{imgIsVisible() && <Widget />}
-
 				<NavBar />
 				{children}
 			</section>
