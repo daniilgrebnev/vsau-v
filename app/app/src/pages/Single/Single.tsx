ff1e92e8af05b30afa447217fb99e01babde3b2f
daniilgrebnev <danilgrebneff@yandex.ru>
2025-03-03 02:21:43 +0300

    add

--- app/app/src/pages/Single/Single.tsx
+++ app/app/src/pages/Single/Single.tsx
@@ -7,19 +7,16 @@ import './single.css'
 export const Single = () => {
 	const { id } = useParams()
 	const [content, setContent] = useState<string | null>(null)
-	const [title, setTitle] = useState<string | null>(null)
 
 	useEffect(() => {
 		getPostContent(Number(id)).then(res => {
 			setContent(res.content ?? null)
-			setTitle(res.title ?? null)
 		})
 	}, [id])
 
 	return (
 		<div className=' pl-24 pr-16 py-6 h-full w-full flex items-start gap-4'>
-			<div className='wp-block-page min-h-full p-4 w-[65%] bg-white rounded-[10px]'>
-				<h1 className='wp-block-heading'>{title}</h1>
+			<div className='wp-block-page min-h-full p-4 w-[65%]  rounded-[10px]'>
 				{content && <div dangerouslySetInnerHTML={{ __html: content }} />}
 			</div>
 			<div className='min-h-full w-[35%]  rounded-[10px]'>
--- app/index.php
+++ app/index.php
@@ -2,7 +2,6 @@
 <html lang="ru">
 
 <head>
-
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<link rel="icon" type="image/svg+xml" href="./vite.svg" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -10,7 +9,7 @@
 	<script type="module" crossorigin src="<?php echo get_template_directory_uri(); ?>/app/index.js"></script>
 	<link rel="stylesheet" crossorigin href="<?php echo get_template_directory_uri(); ?>/app/style.css">
 
-
+	<?php wp_head(); ?>
 </head>
 
 <body>
