14fc57cb92b2872b06ebd1c4f8bacab19b1d8621
daniilgrebnev <danilgrebneff@yandex.ru>
2026-02-11 00:52:35 +0300

    f

--- app/index.php
+++ app/index.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<link rel="icon" type="image/svg+xml" href="favicon.svg" />
+	<link rel="icon" type="image/svg+xml" href="./favicon.svg" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>Абитуриентам</title>
 	<script type="module" crossorigin src="<?php echo get_template_directory_uri(); ?>/app/index.js"></script>
